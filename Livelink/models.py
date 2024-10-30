adb71519ce8258ca7cd8fb8279d8a3e61a34db72
isurucis <143800833+isurucis@users.noreply.github.com>
2024-10-30 20:20:40 +0530

    Update models.py
    generate short code

--- Livelink/models.py
+++ Livelink/models.py
@@ -3,6 +3,8 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.utils import timezone
 from django.shortcuts import reverse
+import string
+import random
 
 class LinkCategory(models.Model):
     name = models.CharField(max_length=100)
@@ -12,7 +14,8 @@ class LinkCategory(models.Model):
 
 class Link(models.Model):
     url = models.URLField()
-    short_code = models.SlugField(unique=True)
+    #short_code = models.SlugField(unique=True)
+    short_code = models.CharField(max_length=10, unique=True, blank=True, editable=True)
     title = models.CharField(max_length=255)  
     category = models.ForeignKey(LinkCategory, on_delete=models.SET_NULL, null=True, blank=True)
     created_by = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, editable=False)
@@ -27,6 +30,14 @@ class Link(models.Model):
     clicks = models.PositiveIntegerField(default=0)
     in_bio = models.BooleanField(default=False)
 
+    def generate_unique_short_code(self):
+        length = 6  # or any preferred length
+        charset = string.ascii_letters + string.digits
+        while True:
+            short_code = ''.join(random.choice(charset) for _ in range(length))
+            if not Link.objects.filter(short_code=short_code).exists():
+                return short_code
+            
     def get_full_url(self):
         """Return URL with UTM parameters if enabled."""
         if self.utm_enabled:
